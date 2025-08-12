961f89f9c88b7b481132a62b8bc0241bf53024dc
DivyShikha <divyshikha1890@gmail.com>
2025-08-12 12:28:59 +0530

    Add project files.

---
+++ Furnitures.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileInputOutputOperations
+{
+    internal class Furnitures
+    {
+        
+        public Furnitures() { }
+
+        public Furnitures(string name, int quantity)
+        {
+            
+        }
+        public static void AddFurniture(string filePath)
+        {
+            try
+            {
+                Console.WriteLine("Furniture name: ");
+                string? Name = Console.ReadLine();
+
+                Console.WriteLine("Quantity: ");
+                int? Quantity = Convert.ToInt32(Console.ReadLine());
+
+                string? input = $"{Name}, {Quantity}";
+                File.AppendAllText(filePath, input + Environment.NewLine);
+            }catch(IOException ex)
+            {
+                Console.WriteLine("An error occurred while writing to the file: " +
+                   ex.Message);
+            }catch(Exception ex){
+                Console.WriteLine("An error occurred while writing to the file: " +
+                   ex.Message);
+            }
+        }
+        public static void ViewFurniture(string filepath)
+        {
+            try
+            {
+                if (!File.Exists(filepath))
+                {
+                    Console.WriteLine("No Firniture exist!");
+                }
+                string[] furnitures = File.ReadAllLines(filepath);
+
+                foreach(var furniture in furnitures){
+                    string[] parts = furniture.Split(',');
+                    Console.WriteLine($"Furniture type: {parts[0]}, Quantity: {parts[1]}");
+                }
+
+            }catch(IOException ioEx)
+            {
+                Console.WriteLine($"Some error occured: {ioEx.Message}");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Some error occured: {ex.Message}");
+
+            }
+
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using FileInputOutputOperations;
+
+Console.WriteLine("Hello, World!");
+string filepath = "furnitures.txt";
+
+FileClassDemo(filepath);
+static object FileClassDemo(string filePath)
+{
+    while (true)
+    {
+        Console.WriteLine("\n--- Furniture App System ---");
+        Console.WriteLine("1. Add New Furniture");
+        Console.WriteLine("2. View All Furniture");
+        Console.WriteLine("3. Exit");
+        Console.Write("Choose an option (1-3): ");
+
+        string? choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                Furnitures.AddFurniture(filePath);
+                break;
+            case "2":
+                Furnitures.ViewFurniture(filePath);
+                break;
+            case "3":
+                return Environment.Exit;
+            default:
+                Console.WriteLine("Invalid option. Please try again.");
+                break;
+        }
+    }
+}
